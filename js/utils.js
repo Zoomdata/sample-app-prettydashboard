b033832cdf134ec236188dadcabcbabcb60eb77c
Eduardo Piña <epinaster@gmail.com>
2016-04-22 09:00:02 -0500

    Fixed chart colors on maximization

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,5 @@
 $('ul').on('click', 'li div.card section.card-header a.fullscr', function(){
   $('#modal1').openModal();
-  //Get the widget
-    li = $(this).parentsUntil('ul')[2];
-    chart_id = $(li).attr('id');
-    options = chartOpts[chart_id].getOption();
-    console.log(options);
-    chart = echartObj.init(document.getElementById('modalchart'));
-    chart.setOption(options);
 })
 
 $('ul').on('click', 'li div.card section.card-header a.del', function(){
